1f8d7eb8295909ff2caa6c8b3a453a1901345998
1c <toylucklebo@gmail.com>
2018-11-23 11:35:24 +0800

    im

--- dj-socket/public/chat.js
+++ dj-socket/public/chat.js
@@ -1,6 +1,80 @@
 window.onload = function() {
     const socket = io()
+    function queryEl(q) {
+        return document.querySelector(q)
+    }
+    const loading = queryEl('#loading')
+    const chat = queryEl('#chat')
+    const form = queryEl('#form')
+    const messageInput = queryEl("#input")
+    const msgs = queryEl('#messages')
+    const dj = queryEl('#dj')
+    const typing = queryEl('#typing')
+
+    let name
     socket.on('connect', () => {
-        console.log(' i am on line')
+        loading.style.display = 'none'
+        chat.style.display = 'block'
+        const inputName =  window.prompt("what's your name?")
+        socket.emit('join', inputName,  function () {
+            name = inputName
+            chat.style.visibility = 'visible'
+        })
     })
+
+    socket.on('broadcast', ({ msg }) => {
+        const li = document.createElement('li')
+        li.textContent =  msg
+        li.className = 'confirmed broadcast'
+        msgs.appendChild(li)
+    })
+
+    socket.on('msg', ({name, msg}) => {
+        const li = document.createElement('li')
+        li.textContent =  `${ name } : ${ msg } `
+        li.className = 'confirmed'
+        msgs.appendChild(li)
+    })
+
+    let timer
+    socket.on('typing', (name) => {
+        typing.style.visibility = 'visible'
+        typing.textContent = `${name} is typing`
+        if(timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            typing.style.visibility = 'hidden'
+        }, 1000)
+    })
+
+    function notTyping() {
+        typing.style.visibility = 'hidden'
+    }
+
+    messageInput.oninput = function () {
+        socket.emit('typing')
+    }
+
+    messageInput.onblur = function () {
+        notTyping()
+    }
+
+    form.onsubmit = function() {
+        const value = messageInput.value
+        if(!value.trim()) {
+            return false
+        }
+        const li = document.createElement('li')
+        li.textContent = `me: ${value}`
+        li.className = 'me-say'
+        socket.emit('message', value , function() {
+            li.className ='me-say confirmed'
+        })
+        msgs.appendChild(li)
+        messageInput.value = ''
+        messageInput.focus()
+        return false
+    }
+
 }
--- dj-socket/server.js
+++ dj-socket/server.js
@@ -9,8 +9,49 @@ const server = http.Server(app)
 const sio = require('socket.io')(server)
 
 server.listen(3000)
+
+let host
+let currentSong
+let users = {}
+let count = 0
+
 sio.on('connection', socket => {
-    console.log('---')
+
+    function broadcast(type, msg) {
+        socket.broadcast.emit(type, msg)
+    }
+    socket.on('join', (name, fn) => {
+        users[name] = socket
+        socket.nickname = name
+        broadcast('broadcast', {msg: `${socket.nickname} are joined!`})
+        fn(Date.now())
+    })
+
+    socket.on('message', (msg, fn) => {
+        broadcast('msg', { msg: msg, name: socket.nickname })
+        fn(Date.now())
+    })
+
+    socket.on('typing', data => {
+        broadcast('typing', socket.nickname)
+    })
+
+    socket.on('search', data => {
+
+    })
+
+    socket.on('song', data => {
+
+    })
+
+    socket.on('disconnect', data => {
+        broadcast('broadcast', { msg: `${socket.id} is leaved !`})
+    })
+
+    // confirm text was send
+    socket.on('text', data => {
+
+    })
 })
 
 
