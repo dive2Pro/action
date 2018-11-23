a5e44dc0d48e402a8155e78786b4c8ee4da6857d
1c <toylucklebo@gmail.com>
2018-11-23 13:45:59 +0800

    dj-unfinished

--- dj-socket/public/chat.js
+++ dj-socket/public/chat.js
@@ -1,46 +1,56 @@
-window.onload = function() {
+window.onload = function () {
     const socket = io()
+
     function queryEl(q) {
         return document.querySelector(q)
     }
+
     const loading = queryEl('#loading')
     const chat = queryEl('#chat')
     const form = queryEl('#form')
     const messageInput = queryEl("#input")
     const msgs = queryEl('#messages')
-    const dj = queryEl('#dj')
     const typing = queryEl('#typing')
+
+    // dj
+    const dj = queryEl('#dj')
+    const s = queryEl("#s")
+    const results = queryEl("#results")
+
 
     let name
     socket.on('connect', () => {
         loading.style.display = 'none'
         chat.style.display = 'block'
-        const inputName =  window.prompt("what's your name?")
-        socket.emit('join', inputName,  function () {
+        const inputName = window.prompt("what's your name?")
+        socket.emit('join', inputName, function () {
             name = inputName
             chat.style.visibility = 'visible'
         })
     })
 
-    socket.on('broadcast', ({ msg }) => {
+    socket.on('broadcast', ({msg}) => {
         const li = document.createElement('li')
-        li.textContent =  msg
+        li.textContent = msg
         li.className = 'confirmed broadcast'
         msgs.appendChild(li)
     })
 
     socket.on('msg', ({name, msg}) => {
         const li = document.createElement('li')
-        li.textContent =  `${ name } : ${ msg } `
+        li.textContent = `${name} : ${msg} `
         li.className = 'confirmed'
         msgs.appendChild(li)
+    })
+    socket.on('dj', () => {
+        dj.style.display = 'block'
     })
 
     let timer
     socket.on('typing', (name) => {
         typing.style.visibility = 'visible'
         typing.textContent = `${name} is typing`
-        if(timer) {
+        if (timer) {
             clearTimeout(timer)
         }
         timer = setTimeout(() => {
@@ -60,21 +70,56 @@ window.onload = function() {
         notTyping()
     }
 
-    form.onsubmit = function() {
+    form.onsubmit = function () {
         const value = messageInput.value
-        if(!value.trim()) {
+        if (!value.trim()) {
             return false
         }
         const li = document.createElement('li')
         li.textContent = `me: ${value}`
         li.className = 'me-say'
-        socket.emit('message', value , function() {
-            li.className ='me-say confirmed'
+        socket.emit('message', value, function () {
+            li.className = 'me-say confirmed'
         })
         msgs.appendChild(li)
         messageInput.value = ''
         messageInput.focus()
         return false
     }
+
+    //
+    dj.onsubmit = function () {
+        const value = s.value
+        if (!value.trim()) {
+            return false
+        }
+
+        socket.emit('search', value)
+
+        return false
+    }
+
+    results.onclick = function(ev) {
+        const { dataset } = ev.target
+        console.log(dataset)
+    }
+    socket.on('songs', songs => {
+        results.innerHTML = ''
+        songs.forEach(song => {
+            const li = document.createElement('li')
+            li.className = 'song'
+            li.innerHTML = `
+    <div>
+        <span>${song.name} ~ ${song.producer}</span>
+        <a data-id="${song.name}">Select</a>
+    </div>
+`
+            results.appendChild(li)
+        })
+    })
+
+    socket.on('selected', songUrl => {
+
+    })
 
 }
--- dj-socket/server.js
+++ dj-socket/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
+const request = require('superagent')
 
 const app = express()
 app.use(bodyParser())
@@ -12,7 +13,7 @@ server.listen(3000)
 
 let host
 let currentSong
-let users = {}
+let users = []
 let count = 0
 
 sio.on('connection', socket => {
@@ -20,15 +21,26 @@ sio.on('connection', socket => {
     function broadcast(type, msg) {
         socket.broadcast.emit(type, msg)
     }
+
+    function elect(nextSocket) {
+        if (!host) {
+            host = nextSocket
+            broadcast('broadcast', {msg: `${nextSocket.nickname} is the new dj!`})
+            nextSocket.emit('dj')
+        }
+    }
+
     socket.on('join', (name, fn) => {
-        users[name] = socket
+        users.push(socket)
         socket.nickname = name
+        socket._index = count++
         broadcast('broadcast', {msg: `${socket.nickname} are joined!`})
         fn(Date.now())
+        elect(socket)
     })
 
     socket.on('message', (msg, fn) => {
-        broadcast('msg', { msg: msg, name: socket.nickname })
+        broadcast('msg', {msg: msg, name: socket.nickname})
         fn(Date.now())
     })
 
@@ -36,22 +48,32 @@ sio.on('connection', socket => {
         broadcast('typing', socket.nickname)
     })
 
-    socket.on('search', data => {
-
+    socket.on('search', (q, fn) => {
+        request('http://tinysong.com/s' + encodeURIComponent(q))
+            .query({key: apiKey, format: 'json'})
+            .end((err, res) => {
+                
+            })
     })
 
     socket.on('song', data => {
 
     })
 
-    socket.on('disconnect', data => {
-        broadcast('broadcast', { msg: `${socket.id} is leaved !`})
+    socket.on('disconnect', () => {
+        users.splice(socket._index, 1)
+        if (host == socket) {
+            host = null
+            broadcast('broadcast', {msg: `dj is leaved !`})
+            if (users.length) {
+                elect(users.shift())
+            }
+        } else {
+            broadcast('broadcast', {msg: `${socket.id} is leaved !`})
+        }
+
     })
 
-    // confirm text was send
-    socket.on('text', data => {
-
-    })
 })
 
 
